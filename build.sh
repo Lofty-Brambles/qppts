bba8d7f6bceb0301fff9430dda8b026e7d570005
Prantik Das <prantik.ts@gmail.com>
2025-10-31 01:08:33 +0530

    feat: add repo to basename

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in $(ls slides/*.md | sed 's/slides\/\|\.md//g'); do
-    slidev build "./slides/${file}.md" --base "/${file}/" --out "../dist/${file}/"
+    slidev build "./slides/${file}.md" --base "/$1/${file}/" --out "../dist/${file}/"
 done
