b92affa2050023e2f3d1bca00c672c424e5f0085
Prantik Das <prantik.ts@gmail.com>
2025-12-30 00:32:27 +0530

    feat: add base page

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 for file in $(ls slides/*.md | sed 's/slides\/\|\.md//g'); do
     slidev build "./slides/${file}.md" --base "/$1/${file}/" --out "../dist/${file}/"
 done
+
+cp base.html dist/index.html
