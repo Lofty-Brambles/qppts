6d81a4029903d869383d4b7c9379d2d507ea1322
Prantik Das <prantik.ts@gmail.com>
2026-03-07 17:00:02 +0530

    fix: rewire build script

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ for file in $(ls slides/*.md | sed 's/slides\/\|\.md//g'); do
     slidev build "./slides/${file}.md" --base "/$1/${file}/" --out "../dist/${file}/"
 done
 
-cp base.html dist/index.html
-cp applications.html dist/applications.html
+for file in $(ls pages/*.html | sed 's/pages\/\|\.html//g'); do
+    cp "./pages/${file}.html" "dist/${file}.html"
+done
+
 cp Quantum_Physics-Informed_Neural_Networks.pdf dist/Quantum_Physics-Informed_Neural_Networks.pdf
